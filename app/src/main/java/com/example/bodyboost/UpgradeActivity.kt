3596fe668d4a2f127b157d8786c8988603cf3952
Kallen-Hsu <kallen.hsu82@gmail.com>
2023-07-22 22:33:12 +0800

    update

---
+++ app/src/main/java/com/example/bodyboost/UpgradeActivity.kt
@@ -0,0 +1,11 @@
+package com.example.bodyboost
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class UpgradeActivity: AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_upgrade)
+    }
+}
