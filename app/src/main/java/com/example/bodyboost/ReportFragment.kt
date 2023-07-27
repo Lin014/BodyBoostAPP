785725167605db7311b0da51135ed578d5b1dbd2
曾筱婷 <hting285@gmail.com>
2023-07-28 00:09:01 +0800

    折線圖更新

--- app/src/main/java/com/example/bodyboost/ReportFragment.kt
+++ app/src/main/java/com/example/bodyboost/ReportFragment.kt
@@ -11,28 +11,29 @@ import android.widget.DatePicker
 import android.widget.EditText
 import androidx.fragment.app.Fragment
 import com.github.mikephil.charting.charts.LineChart
-import com.github.mikephil.charting.components.AxisBase
+import com.github.mikephil.charting.charts.PieChart
 import com.github.mikephil.charting.components.XAxis
-import com.github.mikephil.charting.components.YAxis
 import com.github.mikephil.charting.data.Entry
 import com.github.mikephil.charting.data.LineData
 import com.github.mikephil.charting.data.LineDataSet
-import com.github.mikephil.charting.utils.ColorTemplate
-import com.github.mikephil.charting.formatter.ValueFormatter
-import java.text.DecimalFormat
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter
 import java.text.SimpleDateFormat
 import java.util.*
 
 class ReportFragment : Fragment() {
 
     private lateinit var weightChart: LineChart
-    private lateinit var calorieChart: LineChart
+    private lateinit var caloriesChart: LineChart
+    private lateinit var nutrientChart: PieChart
+
     private lateinit var editTextWeight: EditText
     private lateinit var addWeightButton: Button
 
-    private val weightData = mutableListOf<Entry>()
-    private val testData = mutableListOf<Entry>()
-    private val weights = mutableListOf<Entry>()
+    // date and weight data list
+    private val dateList = mutableListOf<String>()
+    private val weightList = mutableListOf<Float>()
+    // weight chart use data
+    private val weightChartMaxAndMinList = mutableListOf<Float>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -44,161 +45,114 @@ class ReportFragment : Fragment() {
     ): View? {
         val rootView = inflater.inflate(R.layout.fragment_report, container, false)
 
+        // findViewById
         weightChart = rootView.findViewById(R.id.lineChart_weight)
-        calorieChart = rootView.findViewById(R.id.lineChart_calorie)
+        caloriesChart = rootView.findViewById(R.id.lineChart_calorie)
+        nutrientChart = rootView.findViewById(R.id.pieChart_nutrient)
         editTextWeight = rootView.findViewById(R.id.editText_weight)
         addWeightButton = rootView.findViewById(R.id.button1)
 
-        weights.add(Entry(0f, 55f))
-        weights.add(Entry(1f, 54f))
-        weights.add(Entry(2f, 55.5f))
-        weights.add(Entry(3f, 56f))
-        weights.add(Entry(4f, 55f))
+        // api write here
+        // get dateList and weightList data: WeightHistory
 
-        //在沒有資料時設置空白圖表
-        if (weightData.isEmpty()) {
-            SetEmptyChart(weightChart)
-        } else {
-            SetLineChart(weightChart)
-        }
-
-        if (true) {
-            SetEmptyChart(calorieChart)
-        } else {
-            SetLineChart(calorieChart)
-        }
+        weightChartInit()
+        caloriesChartInit()
 
         addWeightButton.setOnClickListener {
-            InputWeight()
+            inputWeight()
         }
 
         // Inflate the layout for this fragment
         return rootView
     }
 
-    private fun SetWeightChart(weights: List<Entry>) {
-        UpdateLineChart(weights, weightChart)
-    }
-    private fun SetCaloriesChart(calories: List<Entry>) {
-        UpdateLineChart(calories, calorieChart)
-    }
-
-    fun SetLineChart(lineChart: LineChart) {
-        lineChart.apply {
-            setDrawGridBackground(false)  //不顯示圖表網格背景
-            setDrawBorders(false)         //不顯示圖表邊框
-            description.isEnabled = false //不顯示 Description Label (預設顯示)
-            legend.isEnabled = false      //不顯示圖例(預設顯示)
-            isDragEnabled = true          //設定只有X軸可以滑動
-            setScaleEnabled(false)        //禁用縮放功能，以確保只有滑動有效
-            setPinchZoom(false)           //禁用縮放手勢
-            isScaleXEnabled = false       //禁用X軸的縮放功能
-            isScaleYEnabled = false       //禁用Y軸的縮放功能
-
-            //X軸設定
-            xAxis.apply {
-                position = XAxis.XAxisPosition.BOTTOM     //X軸顯示位置(預設為上方，分為上方內/外側、下方內/外側及上下同時顯示)
-                textColor = R.color.TextColor             //X軸標籤顏色
-                setDrawLabels(true)                       //顯示X軸標籤
-                textSize = 11f                            //標籤文字大小
-                labelCount = 7                            //標籤數量
-                valueFormatter = DateAxisValueFormatter() //X軸標籤設定
-                setDrawGridLines(false)                   //不顯示格線
-                spaceMin = 20f
-                spaceMax = 20f
-                //granularity = (24 * 60 * 60 * 1000).toFloat() //設置間隔為一天(的秒數)
-            }
-
-            //Y軸設定
-            axisLeft.apply {
-                textColor = R.color.TextColor //Y軸標籤顏色
-                textSize = 11f                //標籤文字大小
-                valueFormatter = IntegerYAxisValueFormatter()
-                granularity = 1f              //標籤間隔
-            }
-
-            axisRight.apply {
-                isEnabled = false //不顯示右側Y軸
-            }
+    private fun weightChartInit() {
+        // init weight chart
+        weightChart.description.isEnabled = false
+        weightChart.axisRight.isEnabled = false
+        weightChart.setNoDataText("目前尚無資料")
+        weightChart.setNoDataTextColor(Color.BLACK)
+        weightChart.setDrawGridBackground(false)
+        weightChart.setDrawBorders(false)
+        weightChart.invalidate()
+        // set weightChartMaxAndMinList
+        for (i in 0..600 step 4) {
+            weightChartMaxAndMinList.add(i.toFloat())
+        }
+        // updateData
+        if (dateList.isNotEmpty()) {
+            setWeightChart(findWeightChartMaxValue(weightList.max()), findWeightChartMinValue(weightList.min()))
         }
     }
 
-    private fun UpdateLineChart(dataList: List<Entry>, lineChart: LineChart) {
-        val dataSet = LineDataSet(dataList, "資料")
-
-        dataSet.color = requireContext().getColor(R.color.Lavender)         //線的顏色
-        dataSet.setCircleColor(requireContext().getColor(R.color.Lavender)) //點的顏色
-        dataSet.setDrawCircleHole(true)                                     //圓點為實心空心(實心-false，空心-true)
-        dataSet.lineWidth = 1.2f                                            //線寬
-        dataSet.setDrawValues(false)                                        //顯示座標點對應 Y軸的數字(預設顯示)
-        dataSet.valueTextSize = 9f                                          //數值文字大小
-        dataSet.valueTextColor = R.color.TextColor                          //文字顏色
-
-        lineChart.data = LineData(dataSet)
-        lineChart.invalidate()
-    }
-
-    private fun SetEmptyChart(lineChart: LineChart) {
-        //沒有資料的空圖表
-        //空圖表資料
-        val entries = mutableListOf<Entry>()
-        entries.add(Entry(System.currentTimeMillis().toFloat(), 0f))
-
-        lineChart.apply {
-            isScaleXEnabled = false        //禁用X軸縮放功能
-            isScaleYEnabled = false        //禁用Y軸縮放功能
-            legend.isEnabled = false       //隱藏圖例
-            description.isEnabled = false  //隱藏簡述
-
-            xAxis.apply {
-                position = XAxis.XAxisPosition.BOTTOM //X軸標籤位置(預設為上方，分為上方內/外側、下方內/外側及上下同時顯示)
-                textColor = R.color.TextColor         //X軸標籤顏色
-                textSize = 11f                        //標籤文字大小
-                setDrawGridLines(false)               //不顯示每個座標點對應X軸的線(預設顯示)
-                axisMinimum = 0f                      //X軸最小值
-                //granularity = 1f
-                labelCount = 7
-                valueFormatter = DateAxisValueFormatter()
-            }
-
-            axisLeft.apply {
-                setDrawAxisLine(true)        //隱藏格線
-                textColor = R.color.TextColor //Y軸標籤顏色
-                textSize = 11f                //標籤文字大小
-                axisMinimum = 0f              //Y軸最小值
-                granularity = 1f
-                labelCount = 5
-                valueFormatter = EmptyYAxisValueFormatter()
-            }
-
-            axisRight.apply {
-                isEnabled = false       //隱藏右側軸線
-                setDrawAxisLine(false)
+    private fun findWeightChartMaxValue(weightListMaxValue: Float): Float {
+        var max = 0f
+        for (value in weightChartMaxAndMinList) {
+            if (value > weightListMaxValue) {
+                max = value.toFloat()
+                break
             }
         }
-
-        val dataSet = LineDataSet(entries, "空資料")
-        //dataSet.color = ColorTemplate.COLORFUL_COLORS[0] //線的顏色
-        dataSet.setDrawCircles(false)                    //不顯示折線圖的圓圈
-        dataSet.setDrawIcons(false)                      //不顯示圖標
-        dataSet.setDrawValues(false)                     //不顯示折線圖的值
-        dataSet.setDrawHighlightIndicators(false)        //不顯示強調部分
-
-        //折線圖資料
-        lineChart.data = LineData(dataSet)
-        //刷新折線圖
-        lineChart.invalidate()
+        return max
     }
 
-    private fun InputWeight() {
+    private fun findWeightChartMinValue(weightListMinValue: Float): Float {
+        var min = 0f
+        for (value in weightChartMaxAndMinList.reversed()) {
+            if (value < weightListMinValue) {
+                min = value.toFloat()
+                break
+            }
+        }
+        return min
+    }
+
+    private fun setWeightChart(yMax: Float, yMin: Float) {
+        // set X
+        weightChart.xAxis.apply {
+            position = XAxis.XAxisPosition.BOTTOM
+            valueFormatter = IndexAxisValueFormatter(dateList)
+            labelCount = dateList.size
+            granularity = 1f
+            setDrawGridLines(false)
+        }
+        // set Y
+        weightChart.axisLeft.apply {
+            axisMaximum = yMax // weight max value
+            axisMinimum = yMin // weight min value
+            labelCount = 5
+        }
+        // add Y data
+        val yList = mutableListOf<Entry>()
+        var index = 0f
+        for (weight in weightList) {
+            yList.add(Entry(index, weight))
+            index += 1f
+        }
+        // set Y data
+        val lineDataSet = LineDataSet(yList, "體重")
+        lineDataSet.color = 0xFFE39D8F.toInt()
+        lineDataSet.setDrawValues(false)
+        lineDataSet.lineWidth = 1.5f
+        lineDataSet.circleRadius = 4f
+        lineDataSet.setCircleColor(0xFFE39D8F.toInt())
+        val lineData = LineData(lineDataSet)
+
+        // update weightChart data
+        weightChart.data = lineData
+        weightChart.setVisibleXRangeMaximum(4f)
+        weightChart.invalidate()
+    }
+
+    private fun inputWeight() {
         val weightString = editTextWeight.text.toString()
         if (weightString.isNotEmpty()) {
             val weight = weightString.toFloat()
-            ShowDatePicker(weight)
+            showDatePicker(weight)
         }
     }
 
-    private fun ShowDatePicker(weight: Float) {
+    private fun showDatePicker(weight: Float) {
         val calendar = Calendar.getInstance()
         val year = calendar.get(Calendar.YEAR)
         val month = calendar.get(Calendar.MONTH)
@@ -210,54 +164,50 @@ class ReportFragment : Fragment() {
                 val selectedCalendar = Calendar.getInstance()
                 selectedCalendar.set(y, m, d)
                 val selectedDateInMillis = selectedCalendar.timeInMillis
-                AddWeightEntry(weight, selectedDateInMillis)
+
+
+                // 使用 SimpleDateFormat 將日期格式化為 'YYYY-MM-DD' 的字串
+                val dateFormat = SimpleDateFormat("yyyy/MM/dd", Locale.getDefault())
+                val selectedDateFormatted = dateFormat.format(selectedCalendar.time)
+
+                // add new date and weight data to weight list
+                // get month and day
+                val dateResult = selectedDateFormatted.substring(5)
+                dateList.add(dateResult)
+                weightList.add(weight)
+                // update weightChart
+                setWeightChart(findWeightChartMaxValue(weightList.max()), findWeightChartMinValue(weightList.min()))
+
+                // api write here
+                // update date and weight data: profile and WeightHistory
             },
             year,
             month,
             day
         )
+        editTextWeight.text.clear()
         datePickerDialog.show()
     }
 
-    private fun AddWeightEntry(weight: Float, dateInMillis: Long) {
-        val entry = Entry(dateInMillis.toFloat(), weight)
-        weightData.add(entry)
-        UpdateLineChart(weightData, weightChart)
-        weightChart.xAxis.valueFormatter = DateAxisValueFormatter()
-        editTextWeight.text.clear()
-    }
-
-    inner class DateAxisValueFormatter : ValueFormatter() {
-        private val dateFormat = SimpleDateFormat("MM/dd", Locale.getDefault())
-
-        override fun getFormattedValue(value: Float): String {
-            val dateInMillis = value.toLong()
-            return dateFormat.format(Date(dateInMillis))
+    private fun caloriesChartInit() {
+        // init calories chart
+        caloriesChart.apply {
+            description.isEnabled = false
+            axisRight.isEnabled = false
+            setNoDataText("目前尚無資料")
+            setNoDataTextColor(Color.BLACK)
+            setDrawGridBackground(false)
+            setDrawBorders(false)
+            invalidate()
         }
     }
 
-    inner class IntegerYAxisValueFormatter : ValueFormatter() {
-        //設定格式為整數
-        private val decimalFormat = DecimalFormat("#")
-
-        override fun getAxisLabel(value: Float, axis: AxisBase?): String {
-            return value.toInt().toString()
-        }
-    }
-
-    inner class EmptyYAxisValueFormatter : ValueFormatter() {
-        override fun getFormattedValue(value: Float): String {
-            // 指定要顯示的標籤值
-            val labels = arrayOf("0", "10", "20", "30", "40", "50")
-
-            // 四捨五入取最接近的整數值
-            val index = (value + 0.5f).toInt()
-
-            return if (index in 0 until 6) {
-                labels[index]
-            } else {
-                "" // 不在指定的標籤範圍內，返回空字串
-            }
+    private fun nutrientChartInit() {
+        nutrientChart.apply {
+            description.isEnabled = false
+            setNoDataText("目前尚無資料")
+            setNoDataTextColor(Color.BLACK)
+            invalidate()
         }
     }
 }
